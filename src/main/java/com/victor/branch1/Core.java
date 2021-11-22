441b169f49bb17bd7f42da90a1d0ea67abc8afc3
haoluo_space <51000477+haoluospace@users.noreply.github.com>
2021-11-22 17:31:39 +0800

    111111111 commit

---
+++ src/main/java/com/victor/branch1/Core.java
@@ -0,0 +1,10 @@
+package com.victor.branch1;
+
+public class Core {
+    public void m1() {
+        System.out.println("macbook pro");
+        System.out.println("macbook pro");
+        System.out.println("macbook pro");
+        System.out.println("macbook pro");
+    }
+}
